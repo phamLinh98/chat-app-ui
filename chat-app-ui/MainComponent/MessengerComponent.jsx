6265c68169dce4e0b73318dc445657a32c4914ef
“linhPham98” <linhthusinh98@gmail>
2024-09-09 01:20:37 +0900

    icon

--- chat-app-ui/MainComponent/MessengerComponent.jsx
+++ chat-app-ui/MainComponent/MessengerComponent.jsx
@@ -3,31 +3,46 @@ import AlertComponent from "../SideComponent/AlertComponent";
 import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import { ItemContext } from "../SideComponent/LayoutConfigComponent";
+import {
+  AliwangwangOutlined,
+  DeleteOutlined,
+  HeartOutlined,
+} from "@ant-design/icons";
 const MessengerComponent = () => {
   const { userId } = useParams();
   const dataGetFromContext = useContext(ItemContext);
   const data = dataGetFromContext.itemsData;
   console.log("data :>> ", data);
 
-  const getUserNameById = (userId) => 
-    data[0]?.children[0]?.info?.find((user) => user.id === userId)?.name || null;
+  const getUserNameById = (userId) =>
+    data[0]?.children[0]?.info?.find((user) => user.id === userId)?.name ||
+    null;
 
   const userName = getUserNameById(userId);
-  console.log('userNameFullName :>> ', userName);
+  console.log("userNameFullName :>> ", userName);
 
   return (
     <>
       <Divider orientation="left">{userName}</Divider>
       <Row justify="start">
         <Col span={6}>
-          <AlertComponent message={"Hello"} type="error" />
-          <AlertComponent message={"Hello"} type="error" />
+            <AlertComponent message={"Hello12345"} type="error" />
+            <div
+              style={{
+                display: "flex",
+                alignItems: "right",
+                gap: "6px",
+              }}
+            >
+              <DeleteOutlined />
+              <AliwangwangOutlined />
+              <HeartOutlined />
+            </div>
         </Col>
       </Row>
       <Divider orientation="right">You</Divider>
       <Row justify="end">
         <Col span={6}>
-          <AlertComponent message={"Hello"} type="success" />
           <AlertComponent message={"Hello"} type="success" />
         </Col>
       </Row>
