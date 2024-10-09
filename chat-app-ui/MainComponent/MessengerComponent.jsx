a44a1810fafa149aaf0ad6eba064e37dd21b5688
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2024-10-09 21:33:56 +0900

    TUAN: Fix bug review

--- chat-app-ui/MainComponent/MessengerComponent.jsx
+++ chat-app-ui/MainComponent/MessengerComponent.jsx
@@ -46,9 +46,7 @@ const MessengerComponent = () => {
       return "User Not Exist";
     }
 
-    const user = infoUserFromTableInfo.find(
-      (item) => String(item.id) === String(userId)
-    );
+    const user = infoUserFromTableInfo.find((item) => String(item.id) === String(userId));
 
     return user
       ? {
@@ -62,8 +60,7 @@ const MessengerComponent = () => {
   // Kiểm tra user nào đang được click
   const userClickNow = getUserNameById(userId);
 
-  const [contextUserLoginAndUserClicked, setContextUserLoginAndUserClicked] =
-    useState(null);
+  const [contextUserLoginAndUserClicked, setContextUserLoginAndUserClicked] = useState(null);
 
   useEffect(() => {
     const fetchDataChat = async () => {
@@ -87,15 +84,11 @@ const MessengerComponent = () => {
     };
 
     fetchDataChat();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [namelogin, userClickNow.namelogin]);
 
   // Tìm kiếm Chat Index từ DB thông qua tài khoản login và user chỉ định từ dashboard
-  const chatIndex = findChatIndex(
-    chatDataFromTableChat,
-    namelogin,
-    userClickNow.namelogin
-  );
+  const chatIndex = findChatIndex(chatDataFromTableChat, namelogin, userClickNow.namelogin);
 
   const { indexfind, setIndex } = useContext(SortedContentsContext);
 
@@ -105,28 +98,50 @@ const MessengerComponent = () => {
     }
   }, [chatIndex, setIndex]);
 
-  const handleSubmit = async () => {
+  // Cách 1:
+  const handleSubmit = () => {
     const newData = {
       id: indexfind,
       avatar: avatar,
       name: namelogin,
-      content: 'Cam on vi tat ca123',
+      content: "Cách 1 >>" + Math.random(),
     };
-  
-    try {
-      const response = await postChatData("/api/add-chat", newData);
-      
-      // Kiểm tra xem response có đúng định dạng không
-      if (response && response.id) { // Hoặc điều kiện kiểm tra khác
-        setContextUserLoginAndUserClicked(response);
-      } else {
-        console.error("Invalid response format", response);
-      }
-    } catch (error) {
-      console.log('error :>> ', error);
-    }
+
+    postChatData("/api/add-chat", newData)
+      .then(({ updatedContents }) => {
+        setContextUserLoginAndUserClicked({ contents: updatedContents });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
-  
+  // Hết cách 1
+
+  // Cách 2:
+  const [addChat, setAddChat] = useState(null);
+
+  useEffect(() => {
+    const fetchDataChat = async () => {
+      if (addChat) {
+        const response = await postChatData("/api/add-chat", addChat);
+        setContextUserLoginAndUserClicked({ contents: response.updatedContents });
+      }
+    };
+
+    fetchDataChat();
+  }, [addChat]);
+
+  const handleSubmitCach2 = () => {
+    const newData = {
+      id: indexfind,
+      avatar: avatar,
+      name: namelogin,
+      content: "Cách 2 >> " + Math.random(),
+    };
+
+    setAddChat(newData);
+  };
+  // Hết cách 2
 
   //Nếu data chưa được fetch thành công thì Loading...
   if (!infoUserFromTableInfo || !contextUserLoginAndUserClicked) {
@@ -135,51 +150,48 @@ const MessengerComponent = () => {
 
   return (
     <>
-      {contextUserLoginAndUserClicked.contents.map((message) => {
-        // thêm index vào đây để tránh cảnh báo
-        const isSender = message.name === namelogin;
-        const isReceiver = message.name === userClickNow.namelogin;
-        if (!isSender && !isReceiver) return null;
-        return (
-          <>
-            <Row key={message.id} justify={isSender ? "start" : "end"}>
-              <Col span={6}>
-                <div
-                  style={{
-                    display: "flex",
-                    gap: "6px",
-                    justifyContent: isSender ? "flex-start" : "flex-end",
-                  }}
-                >
-                  {isReceiver && (
-                    <>
-                      <AlertComponent message={message.content} type="error" />
-                      <SmallAvatarComponent
-                        size={18}
-                        color="red"
-                        icon={namelogin.charAt(0)}
-                        src={avatar}
-                      />
-                    </>
-                  )}
-                  {isSender && (
-                    <>
-                      <SmallAvatarComponent
-                        size={18}
-                        icon={userClickNow.nameshow.charAt(0)}
-                        color="orange"
-                        src={userClickNow.avatar}
-                      />
-                      <AlertComponent message={message.content} type="info" />
-                    </>
-                  )}
-                </div>
-              </Col>
-            </Row>
-          </>
-        );
-      })}
-      <button onClick={handleSubmit}>Click</button>
+      {contextUserLoginAndUserClicked.contents &&
+        contextUserLoginAndUserClicked.contents.map((message) => {
+          // thêm index vào đây để tránh cảnh báo
+          const isSender = message.name === namelogin;
+          const isReceiver = message.name === userClickNow.namelogin;
+          if (!isSender && !isReceiver) return null;
+          return (
+            <>
+              <Row key={message.id} justify={isSender ? "start" : "end"}>
+                <Col span={6}>
+                  <div
+                    style={{
+                      display: "flex",
+                      gap: "6px",
+                      justifyContent: isSender ? "flex-start" : "flex-end",
+                    }}
+                  >
+                    {isReceiver && (
+                      <>
+                        <AlertComponent message={message.content} type="error" />
+                        <SmallAvatarComponent size={18} color="red" icon={namelogin.charAt(0)} src={avatar} />
+                      </>
+                    )}
+                    {isSender && (
+                      <>
+                        <SmallAvatarComponent
+                          size={18}
+                          icon={userClickNow.nameshow.charAt(0)}
+                          color="orange"
+                          src={userClickNow.avatar}
+                        />
+                        <AlertComponent message={message.content} type="info" />
+                      </>
+                    )}
+                  </div>
+                </Col>
+              </Row>
+            </>
+          );
+        })}
+      <button onClick={handleSubmit}>Click Cách 1</button>
+      <button onClick={handleSubmitCach2}>Click Cách 2</button>
     </>
   );
 };
